73f5ce48eb9abeae8c65f1dcc62026320317bb6f
KP <kdp119@scarletmail.rutgers.edu>
2021-10-27 21:04:29 -0400

    Quiz App

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var questions: UILabel!
+    
+    
+    
+    var questionBank = [["5 + 2 = 9", "False"],
+                        ["9 - 2 = 8", "True"],
+                        ["2 + 3 = 5", "True"],
+                        ["1 - 1 = -1", "False"]]
+  
+    
+    var questionNumber = 0
+    
+    
+                        
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    
+    
+    
+    func updateUI()
+    {
+        questions.text =  questionBank[0][0]
     }
 
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton)
+    {
+        
+    }
+    
+    
+    
 }
+    
+    
+
+
 
